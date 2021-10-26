f63cb30f1aad54a00a95a7bac4320c6580eec4ed
Sergey Maslinikov <maslinikovsergey@gmail.com>
2021-10-26 15:50:57 -0400

    toggole

--- src/components/QuestionBlock.js
+++ src/components/QuestionBlock.js
@@ -1,16 +1,35 @@
 import React from 'react';
 
 
-function QuestionBlock() {
+function QuestionBlock( props ) {
 
+    
+   
+    const result = props.answers.map((arg) => {
+        return(
+            <div>
+                <input type="${questions[arg].type}" id="${answer.title}" name="answer" class="d-none" value='${answer.title}' />
+                <label for="{answer.title}" class="d-flex flex-column justify-content-between">
+                    <img class="answerImg" src={arg.url} alt="burger" />
+                    <span>{arg.title}</span>
+                </label>
+            </div>
+        )
+    })
+    
+    console.log(result)
     return (
-        <div class="modal-body  d-flex flex-column align-items-center" id="modalBody">
-            <h5 class="question p-1 p-sm-2 p-md-3 text-center" id="question"></h5>
-            <div class="answers w-100 p-1 p-sm-2 p-md-3" id="answers">
+        <div className="modal-body  d-flex flex-column align-items-center" id="modalBody">
+            <h5 className="question p-1 p-sm-2 p-md-3 text-center" id="question">{props.title}</h5>
+            <div className="answers w-100 p-1 p-sm-2 p-md-3" id="answers">
+                
                 <form action="#" id="formAnswers" class="formAnswers d-flex flex-wrap justify-content-around">
+                    { result }
+               
                 </form>
             </div>
         </div>
+
     )
 }
 
